bae49acee79513c97c9ff8c6c0ea6d49479ffffb
criss-sun <248817095@qq.com>
2020-09-17 00:06:47 +0800

    完成TabControl点击切换商品功能

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :style="activeStyle">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data() {
+    return {
+      // isActive: false,
+    };
+  },
+  computed: {
+    isActive() {
+      return this.$route.path.includes(this.path);
+    },
+    activeStyle() {
+      return this.isActive ? {color: this.activeColor} : {}
+    }
+  },
+  methods: {
+    itemClick() {
+      this.$router.replace(this.path);
+    },
+  },
+};
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item img {
+  width: 20px;
+  height: 20px;
+  margin: 3px 0;
+  vertical-align: middle;
+}
+/* .isActive {
+  color: red;
+} */
+</style>
